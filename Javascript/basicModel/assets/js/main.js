1a14b868ec969a195255a95ab8b8c985493c1aa6
mroya <royamarcio@gmail.com>
2023-07-31 17:52:22 -0300

    class 38 update_exerc

--- Javascript/basicModel/assets/js/main.js
+++ Javascript/basicModel/assets/js/main.js
@@ -1,1 +1,12 @@
-alert(1);
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for(let p of ps){
+    //backgroundColor
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#ffffff';
+}
