0285fe0254ad534dd77ca06415d67d2ea30e6d64
mroya <royamarcio@gmail.com>
2023-08-01 10:35:15 -0300

    exerci class 41

---
+++ Javascript/aula41_exercises1/index.js
@@ -0,0 +1,12 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+// function numeroMaior(x, y){
+//     const maior = Math.max(x, y);
+//     return console.log(maior);
+// }
+
+// numeroMaior(25, 36);
+
+// Usando arrow Function
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10,20));
