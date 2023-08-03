f235775ac8c071a68f1cfe95e94766999307ad7b
mroya <royamarcio@gmail.com>
2023-08-03 11:12:49 -0300

    up class 51

---
+++ Javascript/aula51_functions_returnParam/aula51.js
@@ -0,0 +1,20 @@
+// Usando "return"
+// Retorna um valor
+// Termina a função
+function soma(a, b){
+   return a + b;
+}
+
+function criaMultiplicador(multiplicador){
+   return function(n){
+      return n * multiplicador;
+   };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
